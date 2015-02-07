7910b386bba9cfebb5e2bb19dc7bf0b63c6492e2
Igor Buzhinsky <igor@sparkle>
2015-02-07 20:03:00 +0300

    Final list of cool instances.

--- evaluate.sh
+++ evaluate.sh
@@ -9,23 +9,22 @@ echo "Compiling..."
 ant qbf-automaton-generator-jar
 echo "Evaluating..."
 fsm="qbf/generated-fsm.gv"
-solver_params=""
 
-for suffix in true false; do
+#for suffix in true false; do
+for suffix in true; do
     echo SUFFIX $suffix
     for ((size = $min_size; size <= $max_size; size++)); do
         for ((events = 2; events <= 5; events++)); do
             for ((actions = 2; actions <= 5; actions++)); do
-                name="qbf/testing/fsm_${size}s${events}e${actions}a"
+                name=qbf/testing/fsm_${size}s${events}e${actions}a
                 for cnt in 5 10 20; do
                     for l in 20 30 40 60 80 120 160 240; do
-                        sc_name=${name}_${cnt}_$i.sc
+                        sc_name=${name}_${cnt}_$l.sc
                         for ((formula_num = 1; formula_num <= 5; formula_num++)); do
                             instance_description="s=$size e=$events a=$actions cnt=$cnt l=$l f=$formula_num"
-                            if [ cat cool.log | grep "GOOD \\[VERIFIED\\] $instance_description" ]; then
-                                echo $instance_description
+                            if [[ $(cat qbf/cool.log | grep "HARD .* $instance_description") == "" ]]; then
+                                continue
                             fi
-                            continue
                             ltl_name=$name-$formula_num-$suffix.ltl
                             echo ">>> $instance_description"
                             rm -f "$fsm"
--- qbf/find-cool-instances.sh
+++ qbf/find-cool-instances.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-timeout=300s
+timeout=30s
 
 for ((size = 2; size <= 10; size++)); do
     for ((events = 2; events <= 5; events++)); do
         for ((actions = 2; actions <= 5; actions++)); do
-            if [ ! -f testing/fsm_${size}s${events}e${actions}a.done ]; then
-                echo "NOT DONE s=$size e=$events a=$actions"
-                continue
-            fi
+            #if [ ! -f testing/fsm_${size}s${events}e${actions}a.done ]; then
+            #    echo "NOT DONE s=$size e=$events a=$actions"
+            #    continue
+            #fi
             for cnt in 5 10 20; do
                 for l in 20 30 40 60 80 120 160 240; do
                     rm -f generated-fsm.gv
                     timeout -s TERM $timeout java -jar ../jars/sat-builder.jar testing/fsm_${size}s${events}e${actions}a_${cnt}_$l.sc --result generated-fsm.gv --size $size 2>/dev/null
-                    if [ -f generated-fsm.gv ]; then
-                        fsm_complete=$(java -jar ../jars/completeness-checker.jar generated-fsm.gv)
-                    fi
+                    #if [ -f generated-fsm.gv ]; then
+                    #    fsm_complete=$(java -jar ../jars/completeness-checker.jar generated-fsm.gv)
+                    #fi
                     for ((formula_num = 1; formula_num <= 5; formula_num++)); do
                         report_str="s=$size e=$events a=$actions cnt=$cnt l=$l f=$formula_num"
                         if [ -f generated-fsm.gv ]; then
-                            if [[ $fsm_complete != "COMPLETE" ]]; then
-                                echo "GOOD[NOT COMPLETE] $report_str"
-                                continue
-                            fi
+                            #if [[ $fsm_complete != "COMPLETE" ]]; then
+                            #    echo "GOOD[NOT COMPLETE] $report_str"
+                            #    continue
+                            #fi
                             verified=$(java -jar verifier.jar generated-fsm.gv $size testing/fsm_${size}s${events}e${actions}a-$formula_num-true.ltl | wc -l)
                             if ((verified == formula_num)); then
-                                echo "BAD [  VERIFIED  ] $report_str"
+                                echo "EASY [VERIFIED] $report_str"
                             else
-                                echo "GOOD[NOT VERIFIED] $report_str"
+                                echo "HARD [NOT VERIFIED] $report_str"
                             fi
                         else 
-                            echo "GOOD[ TIME LIMIT ] $report_str"
+                            echo "HARD [TIME LIMIT] $report_str"
                         fi
                     done
                 done
