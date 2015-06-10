22668bdc082fa851791c687659ea1b3f15243878
Igor Buzhinsky <igor@sparkle>
2015-06-10 23:54:37 +0300

    Some explorations concerning randltl operator priorities.

--- qbf/walkinshaw-run-jar-cvs.sh
+++ qbf/walkinshaw-run-jar-cvs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-states=18
-cd .. && ant qbf-automaton-generator-jar && cd qbf && java -ea -Xms2G -Xmx4G -jar ../jars/qbf-automaton-generator.jar walkinshaw/cvs.sc --ltl walkinshaw/cvs.ltl --size $states --eventNumber 16 --eventNames setfiletype,initialise,connect,login,changedir,listfiles,logout,disconnect,makedir,delete,appendfile,retrievefile,listnames,rename,storefile,rmdir --actionNumber 0 --varNumber 0 --timeout 10000 --result generated-fsm.gv --strategy COUNTEREXAMPLE --completenessType NO_DEAD_ENDS --satSolver LINGELING
+states=17
+cd .. && ant qbf-automaton-generator-jar && cd qbf && java -ea -Xms2G -Xmx4G -jar ../jars/qbf-automaton-generator.jar walkinshaw/cvs.sc --ltl walkinshaw/cvs.ltl --size $states --eventNumber 16 --eventNames setfiletype,initialise,connect,login,changedir,listfiles,logout,disconnect,makedir,delete,appendfile,retrievefile,listnames,rename,storefile,rmdir --actionNumber 0 --varNumber 0 --timeout 1000000 --result generated-fsm.gv --strategy COUNTEREXAMPLE --completenessType NO_DEAD_ENDS --satSolver LINGELING
