85034f63de906f07ec0dde26b9bd79d2a75deeaf
Vladimir Ulyantsev <ulyantsev@users.noreply.github.com>
2015-09-11 16:55:46 +0300

    in progress

--- py/mzn-complete-testing.py
+++ py/mzn-complete-testing.py
@@ -8,11 +8,11 @@ import sys
 import re
 import subprocess
 import json
+import string
 
 COMMAND_TEMPLATE = 'java -Xmx512M -Xss128M -jar %s %s'
 
 MZN_TEMPLATE = 'mzn-cpx.bat exact_complete_EFSM_model.mzn -d %s -o %s --soln-sep ""'
-
 
 RESULTS_ROOT = 'results'
 
@@ -27,17 +27,30 @@ MG_PATH = 'jars/minizinc-generator.jar'
 MG_PARAMS_TEMPLATE = '%(scenarios_path)s -s %(size)s -o %(output)s'
 
 BT_PATH = 'jars/qbf-automaton-generator.jar'
-BT_PARAMS_TEMPLATE = '%(path)s --size %(size)d --eventNumber %(ecnt)d --eventNames %(events)s --actionNumber %(acnt)d --actionNames %(actions)s --varNumber %(vcnt)d --timeout %(timeout)d --result %(output)s --strategy BACKTRACKING --ensureCoverageAndWeakCompleteness'
+BT_PARAMS_TEMPLATE = '%(path)s --size %(size)d --eventNumber %(ecnt)d --eventNames %(events)s --actionNumber %(acnt)d --actionNames %(actions)s --varNumber %(vcnt)d --timeout %(timeout)d --result %(output)s --strategy BACKTRACKING --ensureCoverageAndWeakCompleteness --log %(log)s'
 
 TR_PERSENT, EVENTS_COUNT, ACTIONS_COUNT, VARS_COUNT, MIN_ACTIONS, MAX_ACTIONS = 50, 4, 2, 1, 1, 2
 MIN_LEN_C, MAX_LEN_C = 1, 3
 
-LAUNCHES_COUNT = 10
+LAUNCHES_COUNT = 1
 SIZES = [5, 10]
 
 SUM_LEN_MULT = [50]
 
-TIMEOUT = 3600
+TIMEOUT = 600
+
+def get_properties_str():
+    vals = ['Transition persent: %d' % TR_PERSENT,
+            'Events count: %d' % EVENTS_COUNT,
+            'Actions count: %d' % ACTIONS_COUNT,
+            'Variables count: %d' % VARS_COUNT,
+            'Minimum actions count in transition: %d' % MIN_ACTIONS,
+            'Maximum actions count in transition: %d' % MAX_ACTIONS,
+            'Launches count per experiment: %d' % LAUNCHES_COUNT,
+            'Used EFSM sizes: ' + str(SIZES),
+            'Used summary scenarios lengths mult (size * i): ' + str(SUM_LEN_MULT),
+            'JAR execution template: ' + COMMAND_TEMPLATE]
+    return '\n'.join(vals)
 
 def generate_automaton(automaton_path, size):
     ag_params = {'size' : size,
@@ -62,52 +75,65 @@ def generate_scenarios(scenarios_path, automaton_path, size, sum_length):
     os.system(sg_command)
 
 def generate_mzn(scenarios_path, size, output_path):
-    gm_params = {'scenarios_path' : scenarios_path,
+    mg_params = {'scenarios_path' : scenarios_path,
                  'size' : size,
                  'output' : output_path}
-    gm_params_str = SG_PARAMS_TEMPLATE_COVER % gm_params
-    gm_command = COMMAND_TEMPLATE % (MG_PATH, gm_params_str)
+    mg_params_str = MG_PARAMS_TEMPLATE % mg_params
+    mg_command = COMMAND_TEMPLATE % (MG_PATH, mg_params_str)
 
-    os.system(gm_command)
+    os.system(mg_command)
 
-
-def build_automaton(command, result_path, scenarios_path):
+def solve_backtracking(size, dir_path, result_path, scenarios_path):
+    log_path = os.path.join(dir_path, 'backtracking.log')
+    bt_params = {'path': scenarios_path,
+                 'size': size,
+                 'ecnt': EVENTS_COUNT,
+                 'events': '"%s"' % ', '.join(list(string.ascii_uppercase[0:EVENTS_COUNT])),
+                 'acnt': ACTIONS_COUNT,
+                 'actions': '"%s"' % ', '.join(['z%d' % i for i in xrange(0, ACTIONS_COUNT)]),
+                 'vcnt': VARS_COUNT,
+                 'timeout': TIMEOUT,
+                 'output': result_path,
+                 'log': log_path}
+    bt_params_str = BT_PARAMS_TEMPLATE % bt_params
+    bt_command = COMMAND_TEMPLATE % (BT_PATH, bt_params_str)
     
-    builder_command = command % (scenarios_path, result_path)
-    # start time
     start_time = datetime.datetime.now()
-    os.system(builder_command)
-    # end time
+    os.system(bt_command)
     end_time = datetime.datetime.now() 
-    
-    os.system('python postprocess.py ' + result_path)
-
     return end_time - start_time
 
-def launch(dir_path, builder_path, size, sum_length):
+def solve_mzn(data_path, result_path):
+    builder_command = MZN_TEMPLATE % (data_path, result_path)
+
+    start_time = datetime.datetime.now()
+    os.system(builder_command)
+    end_time = datetime.datetime.now() 
+    
+    # ??
+    # os.system('python postprocess.py ' + result_path)
+
+    return (end_time - start_time).micro
+
+def launch(dir_path, size, sum_length):
     automaton_path = os.path.join(dir_path, 'automaton.gv')
     generate_automaton(automaton_path, size)
 
     scenarios_path = os.path.join(dir_path, 'scenarios')
-    sc_count = generate_scenarios(scenarios_path, automaton_path, size, sum_length)
+    generate_scenarios(scenarios_path, automaton_path, size, sum_length)
     
-    result_path = os.path.join(dir_path, 'result.gv')
-    was_found, tree_size, function_ex_time = build_automaton(builder_path, size, dir_path, result_path, scenarios_path)
+    mzn_data_path = os.path.join(dir_path, 'data.mzn')
+    generate_mzn(scenarios_path, size, mzn_data_path)
 
-    compl_path = os.path.join(dir_path, 'completeness-result')
-    is_complete = check_completeness(compl_path, result_path)
+    mzn_result_path = os.path.join(dir_path, 'mzn-result.gv')
+    mzn_time = solve_mzn(mzn_data_path, mzn_result_path)
+
+    #compl_path = os.path.join(dir_path, 'completeness-result')
+    #is_complete = check_completeness(compl_path, result_path)
 
     result = {'size' : size,
-              'sc_count' : sc_count,
               'sum_length' : sum_length,
-              'tree_size' : tree_size,
-              'function_ex_time' : function_ex_time,
-              'result' : was_found,
-              'time' : function_ex_time,
-              'is_complete' : is_complete,
-              'is_isomorph' : is_isomorph,
-              'forward_cc_persent' : forward_cc_persent,
-              'backward_cc_persent': backward_cc_persent}
+              'mzn_time' : mzn_time}
     
     return result
 
@@ -120,19 +146,19 @@ def main():
     results_dir = os.path.join(RESULTS_ROOT, time_str)
     os.mkdir(results_dir)
 
-    print >>open(results_dir + '/experiment-properties', 'w'), get_properties_str(builder_path)
+    print >>open(results_dir + '/experiment-properties', 'w'), get_properties_str()
     
     results = []
     for automaton_size in SIZES:
         cur_size_dir = "%s/%02d-states" % (results_dir, automaton_size)
         os.mkdir(cur_size_dir)
-        for sum_length in automaton_size * SUM_LEN_MULT:
+        for sum_length in [i * automaton_size for i in SUM_LEN_MULT]:
             cur_length_dir = "%s/%04d-sum" % (cur_size_dir, sum_length)
             os.mkdir(cur_length_dir)
             for i in xrange(LAUNCHES_COUNT):
                 launch_dir = "%s/%02d" % (cur_length_dir, i)
                 os.mkdir(launch_dir)
-                res = launch(launch_dir, builder_path, automaton_size, sum_length)
+                res = launch(launch_dir, automaton_size, sum_length)
                 results.append(res)
                 print res
     
