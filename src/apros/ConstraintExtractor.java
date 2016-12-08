7702198c749e51404f5c40fa1bdf6c5a63503320
Igor Buzhinsky <buzhinsky@anomaly>
2016-12-08 10:44:05 +0200

    Bugfix in constraint-based model generation.

--- src/apros/ConstraintExtractor.java
+++ src/apros/ConstraintExtractor.java
@@ -4,9 +4,7 @@ package apros;
  * (c) Igor Buzhinsky
  */
 
-import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -21,7 +19,7 @@ public class ConstraintExtractor {
         final StringBuilder sb = new StringBuilder();
         final String inputLine = String.join(", ",
                 conf.inputParameters.stream().map(p -> "CONT_INPUT_" + p.traceName())
-                .collect(Collectors.toList()));
+                        .collect(Collectors.toList()));
         sb.append("MODULE PLANT(" + inputLine + ")\n");
         sb.append("VAR\n");
         for (Parameter p : conf.outputParameters) {
@@ -61,7 +59,7 @@ public class ConstraintExtractor {
                 + (next ? ")" : "") + " in " + range;
     }
 
-    private static void add1DConstraints (Configuration conf, List<String> initConstraints, List<String> transConstraints, Dataset ds) {
+    private static void add1DConstraints(Configuration conf, List<String> initConstraints, List<String> transConstraints, Dataset ds) {
         for (Parameter p : conf.outputParameters) {
             final Set<Integer> indices = new TreeSet<>();
             for (List<double[]> trace : ds.values) {
@@ -75,7 +73,8 @@ public class ConstraintExtractor {
         }
     }
 
-    private static void add2DConstraints (Configuration conf, List<String> initConstraints, List<String> transConstraints, Dataset ds) {
+    private static void add2DConstraints(Configuration conf, List<String> initConstraints,
+                                         List<String> transConstraints, Dataset ds) {
         for (int i = 0; i < conf.outputParameters.size(); i++) {
             final Parameter pi = conf.outputParameters.get(i);
             for (int j = 0; j < i; j++) {
@@ -94,19 +93,17 @@ public class ConstraintExtractor {
                     }
                 }
                 for (List<String> list : Arrays.asList(initConstraints, transConstraints)) {
+                    final boolean next = list == transConstraints;
                     final Function<Parameter, String> varName = p -> {
-                        String res = "output_" + p.traceName();
-                        if (list == transConstraints) {
-                            res = "next(" + res + ")";
-                        }
-                        return res;
+                        final String res = "output_" + p.traceName();
+                        return next ? ("next(" + res + ")") : res;
                     };
-                    
+
                     final List<String> optionList = new ArrayList<>();
                     for (Map.Entry<Integer, Set<Integer>> implication : indexPairs.entrySet()) {
                         final int index1 = implication.getKey();
                         optionList.add(varName.apply(pi) + " = " + index1 + " & "
-                                + interval(implication.getValue(), pj, false));
+                                + interval(implication.getValue(), pj, next));
                     }
 
                     list.add(String.join(" | ", optionList));
@@ -115,7 +112,7 @@ public class ConstraintExtractor {
         }
     }
 
-    private static void addInputStateConstraints (Configuration conf, List<String> initConstraints, List<String> transConstraints, Dataset ds) {
+    private static void addInputStateConstraints(Configuration conf, List<String> transConstraints, Dataset ds) {
         for (Parameter pi : conf.inputParameters) {
             for (Parameter po : conf.outputParameters) {
                 final Map<Integer, Set<Integer>> indexPairs = new TreeMap<>();
@@ -132,8 +129,8 @@ public class ConstraintExtractor {
                 final List<String> optionList = new ArrayList<>();
                 for (int index1 = 0; index1 < pi.valueCount(); index1++) {
                     optionList.add("CONT_INPUT_" + pi.traceName()
-                        + " in " + pi.nusmvInterval(index1)
-                        + (indexPairs.get(index1).isEmpty() ? ""
+                            + " in " + pi.nusmvInterval(index1)
+                            + (indexPairs.get(index1).isEmpty() ? ""
                             : (" & " + interval(indexPairs.get(index1), po, true))));
                 }
 
@@ -142,7 +139,7 @@ public class ConstraintExtractor {
         }
     }
 
-    private static void addCurrentNextConstraints (Configuration conf, List<String> initConstraints, List<String> transConstraints, Dataset ds) {
+    private static void addCurrentNextConstraints(Configuration conf, List<String> transConstraints, Dataset ds) {
         for (Parameter p : conf.outputParameters) {
             final Map<Integer, Set<Integer>> indexPairs = new TreeMap<>();
             for (List<double[]> trace : ds.values) {
@@ -161,14 +158,14 @@ public class ConstraintExtractor {
             for (Map.Entry<Integer, Set<Integer>> implication : indexPairs.entrySet()) {
                 final int index1 = implication.getKey();
                 optionList.add("output_" + p.traceName()
-                    + " = " + index1
-                    + (" & " + interval(indexPairs.get(index1), p, true)));
+                        + " = " + index1
+                        + (" & " + interval(indexPairs.get(index1), p, true)));
             }
             transConstraints.add(String.join(" | ", optionList));
         }
     }
 
-    private static void printRes (Configuration conf, List<String> initConstraints, List<String> transConstraints, String outFilename) throws IOException {
+    private static void printRes(Configuration conf, List<String> initConstraints, List<String> transConstraints, String outFilename) throws IOException {
         final StringBuilder sb = new StringBuilder();
         sb.append(plantCaption(conf));
         sb.append("    loop_executed: boolean;\n");
@@ -179,18 +176,17 @@ public class ConstraintExtractor {
             initConstraints.add("TRUE");
         }
         sb.append("    ("
-                    +     String.join(")\n  & (", initConstraints)
-                    + ")\n");
+                + String.join(")\n  & (", initConstraints)
+                + ")\n");
         sb.append("TRANS\n");
         if (transConstraints.isEmpty()) {
             transConstraints.add("TRUE");
         }
         sb.append("    ("
-                    +    String.join(")\n  & (", transConstraints)
-                    + ")\n");
+                + String.join(")\n  & (", transConstraints)
+                + ")\n");
 
-        List<String> outParameters = 
-            conf.outputParameters.stream()
+        final List<String> outParameters = conf.outputParameters.stream()
             .map(p -> "output_" + p.traceName() + " = next(output_" + p.traceName() + ")")
             .collect(Collectors.toList());
 
@@ -214,28 +210,33 @@ public class ConstraintExtractor {
         final List<String> transConstraints = new ArrayList<>();
 
         // 1. overall 1-dimensional constraints
+        // "each output may only have values found in the traces"
         if (OVERALL_1D) {
             add1DConstraints(conf, initConstraints, transConstraints, ds);
         }
         // 2. overall 2-dimensional constraints
+        // "for each pair of outputs, only value pairs found in some trace element are possible"
         if (OVERALL_2D) {
             add2DConstraints(conf, initConstraints, transConstraints, ds);
         }
 
         // 2. 2-dimensional constraints "input -> possible next state"
+        // "for each pair of an input and an output, only output values are possible which occur after
+        // the given input value in some pair of contiguous trace elements"
         // if the input is unknown, then no constraint
-        
         // FIXME do something with potential deadlocks, when unknown
         // input combinations require non-intersecting actions
         if (INPUT_STATE) {
-            addInputStateConstraints(conf, initConstraints, transConstraints, ds);
+            addInputStateConstraints(conf, transConstraints, ds);
         }
-        
+
         // 2. 2-dimensional constraints "current state -> next state"
+        // "for each output, only its value transitions found in some pair of contiguous trace
+        // elements are possible"
         if (CURRENT_NEXT) {
-            addCurrentNextConstraints(conf, initConstraints, transConstraints, ds);
+            addCurrentNextConstraints(conf, transConstraints, ds);
         }
-        
+
         printRes(conf, initConstraints, transConstraints, Utils.combinePaths(directory, "plant-constraints.smv"));
     }
 }
