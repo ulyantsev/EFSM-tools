bad728de784e431dd24768530013915822ea6572
dyukha <dimonbv@gmail.com>
2017-07-11 18:27:24 +0300

    Small refactoring.

--- src/main/plant/AprosBuilderMain.java
+++ src/main/plant/AprosBuilderMain.java
@@ -113,14 +113,6 @@ public class AprosBuilderMain extends MainBase {
             usage = "constraint-based: disable output-input-output constraints")
     private boolean constraintBasedDisableOIO_CONSTRAINTS;
 
-    @Option(name = "--constraintBasedDisableCOMPLEX_FAIRNESS_CONSTRAINTS", handler = BooleanOptionHandler.class,
-            usage = "constraint-based: disable complex fairness constraints, generated on base of constant difference of two values to move from one parameter to another")
-    private boolean constraintBasedDisableCOMPLEX_FAIRNESS_CONSTRAINTS;
-
-    @Option(name = "--constraintBasedDisableMONOTONIC_FAIRNESS_CONSTRAINTS", handler = BooleanOptionHandler.class,
-            usage = "constraint-based: disable fairness constraints, generated on the base of monotonicity")
-    private boolean constraintBasedDisableMONOTONIC_FAIRNESS_CONSTRAINTS;
-
     @Option(name = "--constraintBasedDisableINPUT_STATE", handler = BooleanOptionHandler.class,
             usage = "constraint-based: disable input-state constraints")
     private boolean constraintBasedDisableINPUT_STATE;
@@ -128,10 +120,18 @@ public class AprosBuilderMain extends MainBase {
             usage = "constraint-based: disable current-next constraints")
     private boolean constraintBasedDisableCURRENT_NEXT;
 
+    @Option(name = "--constraintBasedDisableMONOTONIC_FAIRNESS_CONSTRAINTS", handler = BooleanOptionHandler.class,
+            usage = "constraint-based: disable fairness constraints, generated on the base of monotonicity")
+    private boolean constraintBasedDisableMONOTONIC_FAIRNESS_CONSTRAINTS;
+
     public static void main(String[] args) {
         new AprosBuilderMain().run(args, Author.IB,
                 "Toolset for NuSMV plant model synthesis from simulation traces in the Apros format");
     }
+
+    @Option(name = "--constraintBasedDisableCOMPLEX_FAIRNESS_CONSTRAINTS", handler = BooleanOptionHandler.class,
+            usage = "constraint-based: disable complex fairness constraints, generated on base of constant difference of two values to move from one parameter to another")
+    private boolean constraintBasedDisableCOMPLEX_FAIRNESS_CONSTRAINTS;
 
     @Override
     protected void launcher() throws IOException {
